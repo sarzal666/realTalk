57692a72271564492521a84e2124c79de9f35081
sarzixon <msarzala.work@gmail.com>
2022-04-10 23:44:45 +0200

    added SignIn page

--- Client/realtalk-client/src/global/global.css.js
+++ Client/realtalk-client/src/global/global.css.js
@@ -6,17 +6,17 @@ export default createGlobalStyle`
 //     color: ${props => (props.darkMode ? 'white' : 'black')};
 //   }
 
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+//   body {
+//     margin: 0;
+//     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+//       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+//       sans-serif;
+//     -webkit-font-smoothing: antialiased;
+//     -moz-osx-font-smoothing: grayscale;
+//   }
   
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
-  }
+//   code {
+//     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+//       monospace;
+//   }
 `
--- Client/realtalk-client/src/index.js
+++ Client/realtalk-client/src/index.js
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client';
-// import './index.css';
-
 import App from './App';
 import GlobalCss from './global/global.css';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
     <>
+        <CssBaseline />
         <GlobalCss />
         <App tab="home" />
     </>
