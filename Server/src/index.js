ddc2330b5b9f8dd40d0d24101072203513d88f5b
sarzixon <msarzala.work@gmail.com>
2022-04-11 00:32:41 +0200

    added body parser, working on firebase user structure

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,31 @@
 const app = require('./app.js');
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//for CORS policy
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
   console.log('hit!');
+})
+
+app.post('/signIn', (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    result: true,
+    token: 'test token'
+  });
 })
 
